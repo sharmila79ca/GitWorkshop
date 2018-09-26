346a8fe0264abae6d16f62a6077049ca38b54868
OpenSourceSLCC <33298309+OpenSourceSLCC@users.noreply.github.com>
2018-09-26 13:31:50 -0600

    Added gitworkshop.java
    This is the test file that will be used when creating your own repositories.

---
+++ gitworkshop.java
@@ -0,0 +1,17 @@
+import gitworkshop;
+
+public class gitworkshop{
+	public static void main(String[] args){
+		System.out.println(multiply(7,6));
+		System.out.println(add(24, 17));
+	}
+	
+	public static int multiply(int num1, int num2){
+	
+	}
+	
+	public static int add(int num1, int num2){
+	
+	}
+	
+}
